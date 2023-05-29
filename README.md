a3e1c932693245c1c5c9564495ce81825c24949f
Eddie Babbe <ebabbe@Eddies-MBP.home>
2023-05-29 16:29:17 -0700

    first commit, pushing all works thus far

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# exp-vs-trad-stats
-This repository contains code that attempts to determine whether expected or traditional stats better predict future performance 
+# What is more predictive or future performance: Traditional Stats or Expected Stats?
+
+## History
+
+For many years, baseball fans and statisticians only had routine counting stats and those based off of dividing events by opportunities ( # of hits / # of at bats for batting average). But in 2015, with the introduction of Statcast, we now had a new way of quantifying a player's skill. Instead of gauging how often a player gets a hit, we started evaluating how often a player *should* be getting a hit. 
+
+## Expected Stats
+
+Expected stats are determined by examining batted ball data (launch angle of contact, exit velocity of ball off the bat, etc) and determining what that event normally produces in the form of a percentage. For example, if a batted ball has exit velocity of 100 mph and a launch angle of 25 degrees, it produces the following outcomes per Baseball Savant:
+BA: .605 1B%: 1.8 2B%: 25.9 3B%: 4.9 HR %: 28.0
+
+## Adoption
+
+Expected stats were anticipated to help teams determine a player's true performance and value. Sure, a player may be hitting .200, but maybe he is just unlucky? If his batted ball metrics are strong, and he actually has a high Expected Batting Average, then he will definitely play better in the future.  
+People have started to look to these stats to predict how a player will perform in the future, but are these stats actually more indicative of a player's future performance than traditional stats.
+
+## Study
+
+I decided to create an interactive tool, found here, that is capable of of fetching data from 2015 until now, and calculating whether expected stats or traditional stats are more valuable in predicting a player's future performance. The stats used are Batting Average, Slugging Percentage, Weighted On Base Average, and their expected equivalents.
+
+### How it works 
+
+You run this tool by supplying two arguments: **sample_seasons**, which can be one or more seasons used as a baseline in averaging out a player's performance over this time period, and **test_season**, which is a single season you want to compare the averaged result of **sample_seasons** to. From there, the Pearson Coefficient between the **sample_seasons** and **test_season** is calculated, where the higher the value, the stronger the correlation. The results are output in graphical form, with 3 windows and 2 graphs per window, where each graph compares one sample season and statistic to its corresponding test season and statistic.
+
+### Caveats
+
+To be included in the study, a player needed to have at least 502 at bats over the sample season(s) period and 1 at bat over the test season period. 502 at bats qualifies you for a batting title, hence why it was used.  
+Additionally, it is important to note that Baseball Savant does not average out expected stats when you request multiple seasons at a time. So, I went through each season, fetched the expected stat, calucated what percentage of total at bats that season was responsible for, and added ( the stat value x's percentage of total at bats) to a summed stat to find the true average of the expected stat over a period of time. So, if a player had a .300 xBA in 10 at bats in 2019, and a .200 xBA in 20 at bats in 2018, his xBA over that two year period = (.300 * (15/30) + .200 * (15/30) = (.150) + (.100) = .250.  
+Lastly, Baseball Savant's API is not returning data for 2020, which may be because 0 players qualified for a batting title that year due to the COVID-shortened season. So, all calculations do not include that year.
+
+### Using the tool
+
+
+## Results
+
+### Graphs
+
+Here are the graphed results, along with their corresponding Pearson Coefficient's, when comparing a player's 2015-2021 expected and traditional stat performance to their traditional stat performance in 2022:
+![BA](results/ba_vs_xba.png)  
+
+According to the graph, neither BA nor xBA is strongly correlated to future BA performance. However, we can see here that traditional BA has a stronger correlation to future BA performance.
+
+
+![SLG](results/slg_vs_xslg.png)  
+
+According to the graph, neither SLG nor xSLG is strongly correlated to future SLG % performance. However, we can see here that traditional SLG % has a stronger correlation to future SLG % performance.
+
+
+![OBA](results/woba_vs_xwoba.png)  
+
+According to the graph, neither wOBA nor xwOBA is strongly correlated to future wOBA performance. However, we can see here that traditional wOBA has a stronger correlation to future wOBA performance.
+
+### Conclusions
+
+A couple conclusions can be drawn from interpreting these results, which include the total of all complete seasons that have used Statcast data:
+
+* Neither traditional nor expected stats, when viewed in isolation, are great indicators of a player's future performance. These stats cannot be solely relied upon to draw conclusions. It would be better to shrink your scope, and see how well a single player or class of player's stats are correlated to the future, in addition to considering more than just the stats here.
+
+* Expected stats are not more helpful than traditional stats. Though neither stat is strongly correlated to future performance, expected stats, which were thought to usurp traditional stats as more valuable in interpreting a player's performance, is actually *less* helpful than traditional stats in predicting a player's future performance. 
---
+++ expected_comp.py
@@ -0,0 +1,277 @@
+import re
+import matplotlib.pyplot as plt
+import seaborn as sns
+import argparse
+import pandas as pd
+from pybaseball import batting_stats, cache
+import sys
+
+cache.enable()
+
+
+def enforce_years(argument):
+    pattern = re.compile("^([0-9]{4})(-[0-9]{4})?$")
+    if pattern.match(argument):
+        return argument
+    else:
+        raise argparse.ArgumentTypeError(
+            "You must either supply a single season, or multiple, consecutive"
+            " seasons separated by a hyphen.\nEx. 2001-2004"
+        )
+
+
+parser = argparse.ArgumentParser(
+    description=(
+        "A utility that pulls, graphs, and compares historical traditional and"
+        " expected stats in order to determine which is a better indicator of"
+        " future performance\nr^2 will be used to judge which stat has a"
+        " strong, linear correlation to future production"
+    )
+)
+parser.add_argument(
+    "--sample_seasons",
+    help="The season(s) you want to use as a baseline",
+    type=enforce_years,
+    required=True,
+)
+parser.add_argument(
+    "--test_season",
+    help=(
+        "The future season you want to use to see if traditional stats or"
+        " expected stats are a better predictor of"
+    ),
+    type=int,
+    required=True,
+)
+
+args = parser.parse_args()
+is_multiple_seasons = False
+if "-" in args.sample_seasons:
+    is_multiple_seasons = True
+first_year = (
+    args.sample_seasons
+    if "-" not in args.sample_seasons
+    else int(args.sample_seasons[0:4])
+)
+if int(first_year) < 2015:
+    print(
+        "ERROR: Statcast data was introduced in 2015, you cannot search for"
+        " data before this point\n"
+    )
+    sys.exit(1)
+
+
+def compute_expected_stat(player, stat):
+    denom = 0
+    sum_stat = 0
+    results_dict = {}
+    begin_year = (
+        int(args.sample_seasons[0:4])
+        if "-" in args.sample_seasons
+        else int(args.sample_seasons)
+    )
+    end_year = (
+        int(args.sample_seasons[5:9]) + 1
+        if "-" in args.sample_seasons
+        else int(args.sample_seasons) + 1
+    )
+    for year in range(begin_year, end_year):
+        if year == 2020:
+            continue
+        year_stats = batting_stats(year)
+        try:
+            exp_stat = year_stats.loc[year_stats["IDfg"] == player["IDfg"]][
+                stat
+            ].item()
+        except Exception:
+            continue
+
+        if stat == "wOBA":
+            pas = year_stats.loc[year_stats["IDfg"] == player["IDfg"]][
+                "PA"
+            ].item()
+            results_dict[year] = [pas, exp_stat]
+        else:
+            abs = year_stats.loc[year_stats["IDfg"] == player["IDfg"]][
+                "AB"
+            ].item()
+            results_dict[year] = [abs, exp_stat]
+    denom = sum(val[0] for key, val in results_dict.items())
+    for key, val in results_dict.items():
+        pct = val[1]
+        opps = val[0]
+        sum_stat += (opps / denom) * pct
+    return sum_stat
+
+
+def build_comparison(player):
+    prev_BA = (
+        data_begin.loc[data_begin["IDfg"] == player["IDfg"]]["H"].item()
+        / data_begin.loc[data_begin["IDfg"] == player["IDfg"]]["AB"].item()
+    )
+    new_BA = (
+        data_end.loc[data_end["IDfg"] == player["IDfg"]]["H"].item()
+        / data_end.loc[data_end["IDfg"] == player["IDfg"]]["AB"].item()
+    )
+    prev_xBA = compute_expected_stat(player, "xBA")
+
+    prev_SLG = data_begin.loc[data_begin["IDfg"] == player["IDfg"]][
+        "SLG"
+    ].item()
+    new_SLG = data_end.loc[data_end["IDfg"] == player["IDfg"]]["SLG"].item()
+    prev_xSLG = compute_expected_stat(player, "xSLG")
+
+    prev_wOBA = data_begin.loc[data_begin["IDfg"] == player["IDfg"]][
+        "wOBA"
+    ].item()
+    new_wOBA = data_end.loc[data_end["IDfg"] == player["IDfg"]]["wOBA"].item()
+    prev_xwOBA = compute_expected_stat(player, "xwOBA")
+    name = data_begin.loc[data_begin["IDfg"] == player["IDfg"]]["Name"].item()
+    dct = {
+        "Name": name,
+        f"{args.sample_seasons} batting average": prev_BA,
+        f"{args.test_season} batting average": new_BA,
+        f"{args.sample_seasons} expected batting average": prev_xBA,
+        f"{args.sample_seasons} slugging %": prev_SLG,
+        f"{args.test_season} slugging %": new_SLG,
+        f"{args.sample_seasons} expected slugging %": prev_xSLG,
+        f"{args.sample_seasons} weighted on base average": prev_wOBA,
+        f"{args.test_season} weighted on base average": new_wOBA,
+        f"{args.sample_seasons} expected weighted on base average": prev_xwOBA,
+    }
+    if prev_xwOBA == 0:
+        dct = {}
+    return dct
+
+
+data_begin = (
+    batting_stats(args.sample_seasons, qual=502)
+    if not is_multiple_seasons
+    else batting_stats(
+        args.sample_seasons[0:4],
+        end_season=args.sample_seasons[5:9],
+        qual=502,
+        ind=0,
+    )
+)
+expected_cols = [col for col in data_begin if col.startswith("x")]
+actual_cols = [col[1:] for col in expected_cols]
+try:
+    data_end = batting_stats(args.test_season)
+except Exception:
+    print(
+        f"ERROR: Cannot fetch Statcast data for {args.test_season}. Has data"
+        " been published for this year yet?\n"
+    )
+    sys.exit(1)
+
+relevant_players = data_begin.loc[data_begin["IDfg"].isin(data_end["IDfg"])]
+
+player_dicts = []
+for index, row in relevant_players.iterrows():
+    dct = build_comparison(row)
+    if dct:
+        player_dicts.append(dct)
+
+df = pd.DataFrame(player_dicts)
+test_df = pd.concat(
+    [
+        df[f"{args.test_season} batting average"],
+        df[f"{args.sample_seasons} expected batting average"],
+    ],
+    axis=1,
+)
+test_df_a = pd.concat(
+    [
+        df[f"{args.test_season} batting average"],
+        df[f"{args.sample_seasons} batting average"],
+    ],
+    axis=1,
+)
+fig_ba, ax_ba = plt.subplots(1, 2, figsize=(10, 5))
+fig_ba.suptitle(
+    "Batting Average v. Expected Batting Average as indicator of future"
+    " production"
+)
+fig_slg, ax_slg = plt.subplots(1, 2, figsize=(10, 5))
+fig_slg.suptitle(
+    "Slugging % v. Expected Slugging % as indicator of future production"
+)
+fig_oba, ax_oba = plt.subplots(1, 2, figsize=(10, 5))
+fig_oba.suptitle(
+    "Weighted On Base Average v. Expected Weighted On Base Average as indicator"
+    " of future production"
+)
+sns.regplot(
+    df,
+    x=f"{args.sample_seasons} batting average",
+    y=f"{args.test_season} batting average",
+    fit_reg=True,
+    scatter=True,
+    ax=ax_ba[0],
+)
+ax_ba[0].set_title(
+    "Pearson coefficient:"
+    f" {str(round(pd.concat([df[f'{args.test_season} batting average'], df[f'{args.sample_seasons} batting average']], axis=1).corr().iloc[0,1],2))}"
+)
+sns.regplot(
+    df,
+    x=f"{args.sample_seasons} expected batting average",
+    y=f"{args.test_season} batting average",
+    fit_reg=True,
+    scatter=True,
+    ax=ax_ba[1],
+)
+ax_ba[1].set_title(
+    "Pearson coefficient:"
+    f" {str(round(pd.concat([df[f'{args.test_season} batting average'], df[f'{args.sample_seasons} expected batting average']], axis=1).corr().iloc[0,1],2))}"
+)
+sns.regplot(
+    df,
+    x=f"{args.sample_seasons} slugging %",
+    y=f"{args.test_season} slugging %",
+    fit_reg=True,
+    scatter=True,
+    ax=ax_slg[0],
+)
+ax_slg[0].set_title(
+    "Pearson coefficient:"
+    f" {str(round(pd.concat([df[f'{args.test_season} slugging %'], df[f'{args.sample_seasons} slugging %']], axis=1).corr().iloc[0,1],2))}"
+)
+sns.regplot(
+    df,
+    x=f"{args.sample_seasons} expected slugging %",
+    y=f"{args.test_season} slugging %",
+    fit_reg=True,
+    scatter=True,
+    ax=ax_slg[1],
+)
+ax_slg[1].set_title(
+    "Pearson coefficient:"
+    f" {str(round(pd.concat([df[f'{args.test_season} slugging %'], df[f'{args.sample_seasons} expected slugging %']], axis=1).corr().iloc[0,1],2))}"
+)
+sns.regplot(
+    df,
+    x=f"{args.sample_seasons} weighted on base average",
+    y=f"{args.test_season} weighted on base average",
+    fit_reg=True,
+    scatter=True,
+    ax=ax_oba[0],
+)
+ax_oba[0].set_title(
+    "Pearson coefficient:"
+    f" {str(round(pd.concat([df[f'{args.test_season} weighted on base average'], df[f'{args.sample_seasons} weighted on base average']], axis=1).corr().iloc[0,1],2))}"
+)
+sns.regplot(
+    df,
+    x=f"{args.sample_seasons} expected weighted on base average",
+    y=f"{args.test_season} weighted on base average",
+    fit_reg=True,
+    scatter=True,
+    ax=ax_oba[1],
+)
+ax_oba[1].set_title(
+    "Pearson coefficient:"
+    f" {str(round(pd.concat([df[f'{args.test_season} weighted on base average'], df[f'{args.sample_seasons} expected weighted on base average']], axis=1).corr().iloc[0,1],2))}"
+)
+plt.show()
