861d2b591c84bdeef222f1a773bfe58eb0aa39ad
Eddie Babbe <ebabbe@Eddies-MBP.home>
2025-02-19 21:57:34 -0800

    first pass create terraform infra

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "my-aks-rg"
-    storage_account_name = "myterraformstate"
+    storage_account_name = "myterraformstate9422"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
